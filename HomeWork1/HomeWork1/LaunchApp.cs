a2db601fdd87fe77cb829bf551d44eead06de957
ihor tereschenko <igor.tereschenko@gmail.com>
2020-12-19 19:02:28 +0200

    Comment to devel branch

--- HomeWork1/HomeWork1/LaunchApp.cs
+++ HomeWork1/HomeWork1/LaunchApp.cs
@@ -25,6 +25,7 @@ namespace HomeWork1
         /*public void ConfigureServices(IServiceCollection services)
         {
         //This method is optional
+        // Comment for devel branch
         }
         */
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
